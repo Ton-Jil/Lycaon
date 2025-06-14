e30685c513c435a176c52a4d00d6570950bc8a29
Ton Jil <211355862+Ton-Jil@users.noreply.github.com>
2025-06-14 11:10:11 +0000

    期限切れチェック方法を変更

--- bot.py
+++ bot.py
@@ -770,7 +770,7 @@ active_character_key = None
 active_character_display_name = (
     "デフォルト"  # 現在のキャラクター表示名を保持するグローバル変数
 )
-active_cache_name = None
+active_cache = None
 
 
 def _create_chat_session(cache_name: str, history: list):
@@ -791,7 +791,7 @@ def initialize_chat_session(character_key_to_load=None):
     """
     ボット起動時に呼び出され、チャットセッションを初期化または復元する。
     """
-    global shared_chat_session, active_character_key, active_character_display_name, initial_conversation_history_count, active_cache_name
+    global shared_chat_session, active_character_key, active_character_display_name, initial_conversation_history_count, active_cache
 
     if character_key_to_load is None:
         character_key_to_load = get_setting_from_db("current_character_key", "lycaon")
@@ -833,7 +833,7 @@ def initialize_chat_session(character_key_to_load=None):
         except Exception as e:
             print(f"CachedContent の作成中にエラーが発生しました: {e}")
 
-    active_cache_name = system_cache.name
+    active_cache = system_cache
 
     create_table_if_not_exists()  # DBテーブル作成
 
@@ -903,7 +903,7 @@ async def handle_shared_discord_message(
     """
     Discordのメッセージを受け取り、Gemini APIに応答を生成させる (共有・効率化版)
     """
-    global shared_chat_session, active_character_key, active_cache_name
+    global shared_chat_session, active_character_key, active_cache
 
     if not shared_chat_session:
         # ボット起動時に初期化されているはずだが、念のため
@@ -944,10 +944,8 @@ async def handle_shared_discord_message(
             first_api_call_contents.append(image_part)
 
     # キャッシュの期限切れチェック
-    try:
-        cache_obj = client.caches.get(name=active_cache_name)
-    except Exception as e:
-        initialize_chat_session()
+    if active_cache.expire_time < datetime.datetime.now(datetime.timezone.utc):
+        initialize_chat_session(active_character_key)
 
     MAX_ATTEMPTS_FOR_LENGTH = 3  # 初回試行 + 2回の短縮試行
     bot_response_text = ""
